a96076d381526e966e04ba6d48cef30bb7bef531
Tim <tim.lansen@gmail.com>
2017-06-27 09:37:08 +0300

    safe eval() in condition checker

--- python/modules/utils/condition.py
+++ python/modules/utils/condition.py
@@ -8,5 +8,34 @@
 # object is an object that contains all necessary data
 
 
-def condition_is_true(condition, **objects):
-    return eval(condition, objects)
+def condition_is_true(condition: str, **objects):
+    cond = condition.replace('__', '')
+    result = False
+    try:
+        result = eval(cond, objects)
+    except Exception as e:
+        print(e)
+    return result
+
+
+def test():
+    class RootClass:
+        def __init__(self):
+            self.abc = 0
+            self.cc = self.ChildClass1()
+
+        class ChildClass1:
+            def __init__(self):
+                self.abc = 1
+                self.cc = self.ChildClass2()
+
+            class ChildClass2:
+                def __init__(self):
+                    self.abc = 2
+
+    root = RootClass()
+    print(condition_is_true('obj1.abc < obj2.abc or obj2.cc.abc < obj1.cc.abc', **{'obj1': root, 'obj2': root.cc}))
+
+
+if __name__ == '__main__':
+    test()
